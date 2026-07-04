1ea2008b1a9036e044f42e64974e6d97ac1d434a
Alexis Chayan <N00310165@upn.pe>
2026-07-04 12:20:38 -0500

    cliente: agrega entidad Cliente e interfaz IClienteRepository

---
+++ Interfaces/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using T2ProyectoInventariado.Entities;
+
+namespace T2ProyectoInventariado.Interfaces
+{
+    public interface IClienteRepository
+    {
+        List<Cliente> ObtenerTodos();
+        Cliente? ObtenerPorId(int id);
+        void Agregar(Cliente cliente);
+        void Actualizar(Cliente cliente);
+    }
+}
