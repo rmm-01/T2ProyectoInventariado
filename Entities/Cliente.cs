5ed02440046e7ecf8a668ffdf8120bbeb3ece79b
Alexis Chayan <N00310165@upn.pe>
2026-07-04 12:20:38 -0500

    cliente: agrega entidad Cliente e interfaz IClienteRepository

---
+++ Entities/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace T2ProyectoInventariado.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string RucODni { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string Direccion { get; set; } = string.Empty;
+    }
+}
